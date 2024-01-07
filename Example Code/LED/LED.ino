904e025fb5e69b99ffeab7ce4e3f3f6800748b39
Teng Kong Leong <klteng73@gmail.com>
2024-01-07 21:20:49 +0800

    Add files via upload

---
+++ Example Code/LED/LED.ino
@@ -0,0 +1,21 @@
+#include <Engiro.h>
+Engiro Engiro;
+
+void setup() {
+  // put your setup code here, to run once:  
+  Debug.begin(115200);
+  Engiro.Uart_Initialize();
+  Debug.println("START\n");      
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Engiro.Led1_On();     // Turn on LED 1
+  delay(500);           // Delay 500ms
+  Engiro.Led1_Off();    // Turn off LED 1
+  delay(500);           // Delay 500ms
+  Engiro.Led2_On();     // Turn on LED 2
+  delay(500);           // Delay 500ms
+  Engiro.Led2_Off();    // Turn off LED 2
+  delay(500);           // Delay 500ms
+}
